e7bfa98658ae4903dd39ba761c3880442c99079a
Federico Mantoni <federico.mantoni@studio.unibo.it>
2023-11-23 08:14:17 +0100

    finished exercise 91

--- 91-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
+++ 91-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
@@ -1,9 +1,11 @@
 package it.unibo.mvc;
 
 import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
 import it.unibo.mvc.controller.DrawNumberControllerImpl;
 import it.unibo.mvc.model.DrawNumberImpl;
-import it.unibo.mvc.view.DrawNumberSwingView;
+import java.lang.reflect.*;
+import java.util.List;
 
 /**
  * Application entry-point.
@@ -17,15 +19,25 @@ public final class LaunchApp {
      *
      * @param args ignored
      * @throws ClassNotFoundException if the fetches class does not exist
+     * @throws SecurityException
      * @throws NoSuchMethodException if the 0-ary constructor do not exist
      * @throws InvocationTargetException if the constructor throws exceptions
      * @throws InstantiationException if the constructor throws exceptions
      * @throws IllegalAccessException in case of reflection issues
      * @throws IllegalArgumentException in case of reflection issues
      */
-    public static void main(final String... args) {
+    public static void main(final String... args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
         final var model = new DrawNumberImpl();
         final DrawNumberController app = new DrawNumberControllerImpl(model);
-        app.addView(new DrawNumberSwingView());
+       
+        for (final var viewClass : List.of("Swing", "Stdout")) {
+            final var classView = Class.forName("it.unibo.mvc.view.DrawNumber" + viewClass + "View");
+            final var myIstance = classView.getConstructor().newInstance();
+            if (DrawNumberView.class.isAssignableFrom(myIstance.getClass())) {
+                for (int i = 0; i < 3; i++) {
+                    app.addView((DrawNumberView)classView.getConstructor().newInstance());
+                }
+            }
+        }
     }
 }
---
+++ 91-mvc-gui-reflection/src/main/java/it/unibo/mvc/view/DrawNumberStdoutView.java
@@ -0,0 +1,36 @@
+package it.unibo.mvc.view;
+
+
+import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
+import it.unibo.mvc.api.DrawResult;
+
+public class DrawNumberStdoutView implements DrawNumberView{
+
+    private static final String NEW_GAME = "A new game started";
+
+    @Override
+    public void setController(final DrawNumberController observer) {
+        
+    }
+
+    @Override
+    public void start() {
+        
+    }
+
+    @Override
+    public void result(final DrawResult res) {
+        switch (res) {
+            case YOURS_HIGH, YOURS_LOW -> {
+                System.out.println(res.getDescription());
+                return;
+            }
+            case YOU_WON -> System.out.println(res.getDescription() + NEW_GAME);
+            case YOU_LOST -> System.out.println(res.getDescription() + NEW_GAME +"Lost");
+            default -> throw new IllegalStateException("Unknown game state");
+        }
+        System.out.println("Resetting the game");
+    }
+    
+}
